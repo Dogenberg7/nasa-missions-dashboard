ce336980f687173d5dd1c5d76098a81a8759c508
Camilla Pascucci <camilla.pascucci99@gmail.com>
2026-08-28 18:07:51 +0200

    Add launch_sites table to db

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,10 @@
 BEGIN;
 
-
+CREATE TABLE launch_sites (
+    id          TEXT PRIMARY KEY,
+    name        TEXT NOT NULL,
+    locality    TEXT NOT NULL,
+    country     TEXT NOT NULL,
+    latitude    NUMERIC(8, 5) NOT NULL CHECK (latitude  BETWEEN  -90 AND  90),
+    longitude   NUMERIC(8, 5) NOT NULL CHECK (longitude BETWEEN -180 AND 180)
+);
