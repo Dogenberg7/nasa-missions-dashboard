4865da45771da82a18a5d519bdf4dfe19e62d018
Camilla Pascucci <camilla.pascucci99@gmail.com>
2026-08-28 19:05:55 +0200

    Add server startup

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`NASA missions dashboard listening on http://localhost:${port}`);
+})
