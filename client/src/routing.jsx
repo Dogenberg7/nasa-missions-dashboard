a17888eb542d161bcd80d08702ff4913251ba978
Camilla Pascucci <camilla.pascucci99@gmail.com>
2026-09-02 14:14:43 +0200

    Add launch site page

--- client/src/routing.jsx
+++ client/src/routing.jsx
@@ -2,6 +2,7 @@ import {createBrowserRouter} from "react-router-dom";
 import Home from "./routes/Home/Home.jsx";
 import Missions from "./routes/Missions/Missions.jsx";
 import MissionDetail from "./routes/MissionDetail/MissionDetail.jsx";
+import LaunchSites from "./routes/LaunchSites/LaunchSites.jsx";
 
 export const router = createBrowserRouter([
     {
@@ -15,5 +16,9 @@ export const router = createBrowserRouter([
     {
         path: "/missions/:slug",
         element: <MissionDetail/>
+    },
+    {
+        path: "/launch-sites/:id",
+        element: <LaunchSites/>
     }
 ]);
